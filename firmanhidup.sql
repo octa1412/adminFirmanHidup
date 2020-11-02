4b7add63559aa0d2a63e40db6f73dcc008859e82
octa1412 <intan.chen53@gmail.com>
2020-11-02 11:49:01 +0700

    database

--- firmanhidup.sql
+++ firmanhidup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2020 at 05:06 AM
+-- Generation Time: Nov 02, 2020 at 05:48 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.28
 
@@ -90,30 +90,6 @@ CREATE TABLE `video` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `video`
---
-
-INSERT INTO `video` (`IdVideo`, `NamaVideo`, `IdKategori`, `SourceVideo`, `SourceGambar`) VALUES
-(10, 'semua ada 1', 1, '2010282020_09_10_11_36_51.mp4', NULL),
-(11, 'semua ada', 2, '2010282020_09_10_11_36_511.mp4', NULL),
-(12, 'hhh', 3, '2010282020_09_10_11_38_30.mp4', NULL),
-(13, 'kat1', 1, '2010282020_09_10_11_37_38.mp4', NULL),
-(14, 'kkkk', 2, '2010282020_09_10_11_31_42.mp4', NULL),
-(15, 'asdada', 3, '2010282020_09_10_11_37_381.mp4', NULL),
-(16, 'semua ada', 1, '2010282020_09_10_11_38_301.mp4', NULL),
-(17, 'kkkk', 2, '2010282020_09_10_11_37_382.mp4', NULL),
-(18, 'semua ada', 3, '2010282020_09_10_11_37_383.mp4', NULL),
-(19, 'semua ada', 1, '2010282020_09_10_11_37_384.mp4', NULL),
-(20, 'asd', 2, '2010282020_09_10_11_37_385.mp4', NULL),
-(21, 'jjj', 3, '2010282020_09_10_11_37_386.mp4', NULL),
-(22, 'kat1', NULL, '2010282020_09_10_11_36_512.mp4', NULL),
-(23, 'asd', NULL, '2010282020_09_10_11_37_387.mp4', NULL),
-(24, 'kat1', NULL, '2010282020_09_10_11_37_388.mp4', NULL),
-(25, 'kat1', NULL, '2010282020_09_10_11_37_389.mp4', NULL),
-(26, 'semua ada', NULL, '2010282020_09_10_11_37_3810.mp4', NULL),
-(27, 'Video 2', 2, '2010302020_09_10_11_39_02.mp4', NULL);
-
---
 -- Indexes for dumped tables
 --
 
@@ -156,7 +132,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `IdVideo` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `IdVideo` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
